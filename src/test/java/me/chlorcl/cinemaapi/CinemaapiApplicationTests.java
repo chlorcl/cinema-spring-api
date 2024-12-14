ef11a360999b59ba3b0c56bf0eb267b545ce119b
chlorcl <81020323+chlorcl@users.noreply.github.com>
2024-12-14 12:35:17 +0100

    First commit

---
+++ src/test/java/me/chlorcl/cinemaapi/CinemaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chlorcl.cinemaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
